edef7725ece7c3b7c849fd5ab709b43937bcd046
joaovguerra <guerra.joaovictor05@gmail.com>
2025-03-21 23:49:12 -0300

    sentença e blocos

---
+++ JS/aula3.20.js
@@ -0,0 +1,29 @@
+{
+    console.log("Passo #1")
+    console.log("Passo #2")
+    console.log("Passo #3")
+    console.log("Passo #4")
+}
+
+{
+    console.log("Passo #5")
+    console.log("Passo #6")
+    console.log("Passo #7")
+    console.log("Passo #8")
+}
+
+{
+  {
+    {
+        {
+            {
+              
+            
+                
+            }
+        }
+    }
+  }
+}
+
+console.log("fim")
