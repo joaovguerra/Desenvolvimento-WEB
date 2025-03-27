9be2a5b7f2ac858e8c69c2dab4c437c72d5a77e6
joaovguerra <guerra.joaovictor05@gmail.com>
2025-03-27 18:41:40 -0300

    aula 5 dados - ex. área circunferência

---
+++ JS/aula3.27.dadosAreaCircun.js
@@ -0,0 +1,7 @@
+// PI * raio * raio
+// PI = 3.14
+
+const PI = 3.141592
+let raio = 10
+let areaCircunferencia = PI * raio * raio
+console.log("O valor da área da é : " + areaCircunferencia + "m².")
