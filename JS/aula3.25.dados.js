db0f4e8b01559fe3821291f51328efdc5400b82f
joaovguerra <guerra.joaovictor05@gmail.com>
2025-03-27 15:29:02 -0300

    aula dados 3 - typeof ( number , boolean , string

---
+++ JS/aula3.25.dados.js
@@ -0,0 +1,15 @@
+// no JS , independente se é número inteiro ou decimal , ele entende com number ( Número)
+
+let idade = 31
+console.log(typeof idade)
+
+let salario = 4500.55
+console.log(typeof salario)
+
+// no JS , independente se é número inteiro ou decimal , ele entende com number ( Número)
+
+let estaChovendo = true
+console.log(typeof estaChovendo)
+
+let nome = "Joao"
+console.log(typeof nome)
